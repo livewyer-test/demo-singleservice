3bc7a83677a8468ad893c55109e29ce36643db14
David O'Dwyer <david@livewyer.com>
2019-03-28 10:30:53 +0000

    Up

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example v1.2"
+	title := "Jenkins X golang http example v1.3"
 
 	from := ""
 	if r.URL != nil {
