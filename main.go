983e72d8470f827605a7446289b321c70685d9f7
Ger Malone <gmalone@orw-gmalone-ct7.wv.mentorg.com>
2019-04-01 08:00:09 -0700

    New Version

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example v3.0"
+	title := "Jenkins X golang http example  v3.0"
 
 	from := ""
 	if r.URL != nil {
